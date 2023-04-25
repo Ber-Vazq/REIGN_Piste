086ba1342e6ed713ad90e542ee2dac068180cb07
Ber-Vazq <bvaq20@hotmail.com>
2023-04-25 12:16:00 -0500

    minor

--- Reign_Piste/Assets/ASSETS/SCRIPTS/Player/playerController.cs
+++ Reign_Piste/Assets/ASSETS/SCRIPTS/Player/playerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
 {
